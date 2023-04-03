4c9f041e286c5b25dae6469aebda294625a37717
Julia Nitsch <julia.nitsch@gmail.com>
2023-04-03 13:14:26 +0200

    Tippfehler Korrector in 02_04 loesung

--- loesungen/modul_2/02_04_lsg.py
+++ loesungen/modul_2/02_04_lsg.py
@@ -1,5 +1,5 @@
 wahre_variable = True
 falsche_variable = False
 
-print('Inhalt wahre_vairable', wahre_variable)
+print('Inhalt wahre_variable', wahre_variable)
 print('Inhalt falsche_variable', falsche_variable)
